6d83a9a1e55b8e7904b4e94ba8a847722504a431
Vasilios Magriplis <etameson@gmail.com>
2020-06-04 22:02:35 +1000

    First cut of SlimReadWriteLock

--- src/Boring32/src/Async/SlimReadWriteLock.cpp
+++ src/Boring32/src/Async/SlimReadWriteLock.cpp
@@ -6,4 +6,58 @@ namespace Boring32::Async
 {
 	SlimReadWriteLock::~SlimReadWriteLock()
 	{ }
+
+	SlimReadWriteLock::SlimReadWriteLock()
+	:	m_threadOwningExclusiveLock(0)
+	{
+		InitializeSRWLock(&m_srwLock);
+	}
+
+	bool SlimReadWriteLock::TryAcquireSharedLock()
+	{
+		return TryAcquireSRWLockShared(&m_srwLock);
+	}
+
+	bool SlimReadWriteLock::TryAcquireExclusiveLock()
+	{
+		DWORD currentThreadId = GetCurrentThreadId();
+		if (m_threadOwningExclusiveLock == currentThreadId)
+			return true;
+		if (TryAcquireSRWLockExclusive(&m_srwLock))
+		{
+			m_threadOwningExclusiveLock = currentThreadId;
+			return true;
+		}
+		return false;
+	}
+
+	void SlimReadWriteLock::AcquireSharedLock()
+	{
+		AcquireSRWLockShared(&m_srwLock);
+	}
+
+	void SlimReadWriteLock::AcquireExclusiveLock()
+	{
+		DWORD currentThreadId = GetCurrentThreadId();
+		if (m_threadOwningExclusiveLock != currentThreadId)
+		{
+			AcquireSRWLockExclusive(&m_srwLock);
+			m_threadOwningExclusiveLock = currentThreadId;
+		}
+	}
+
+	void SlimReadWriteLock::ReleaseSharedLock()
+	{
+		ReleaseSRWLockShared(&m_srwLock);
+	}
+
+	void SlimReadWriteLock::ReleaseExclusiveLock()
+	{
+		DWORD currentThreadId = GetCurrentThreadId();
+		if (m_threadOwningExclusiveLock == currentThreadId)
+		{
+			ReleaseSRWLockExclusive(&m_srwLock);
+			m_threadOwningExclusiveLock = 0;
+		}
+	}
 }
