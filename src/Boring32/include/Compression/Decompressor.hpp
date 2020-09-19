ce46ac51fe53af61e7e8cb558cf44f63dfbcf0f0
Vasilios Magriplis <etameson@gmail.com>
2020-09-19 14:16:00 +1000

    Add comments

--- src/Boring32/include/Compression/Decompressor.hpp
+++ src/Boring32/include/Compression/Decompressor.hpp
@@ -28,8 +28,18 @@ namespace Boring32::Compression
 			/// <returns>The algorithm used by this decompressor.</returns>
 			[[nodiscard]] virtual CompressionType GetType() const;
 
+			/// <summary>
+			///		Returns the uncompressed size, in bytes, of the compressed buffer specified in the parameter.
+			/// </summary>
+			/// <param name="buffer">The buffer to determine the uncompressed size of.</param>
+			/// <returns>The uncompressed size, in bytes, of the compressed buffer specified in the parameter</returns>
 			[[nodiscard]] virtual size_t GetDecompressedSize(const std::vector<std::byte>& compressedBuffer) const;
 
+			/// <summary>
+			///		Returns a buffer that is the decompressed data of the input argument, buffer.
+			/// </summary>
+			/// <param name="buffer">The buffer to decompress.</param>
+			/// <returns>The decompressed buffer.</returns>
 			[[nodiscard]] virtual std::vector<std::byte> DecompressBuffer(const std::vector<std::byte>& compressedBuffer);
 
 		protected:
