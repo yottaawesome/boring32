e7b071157c5915802bd01e4965c86ed5261fb37d
Vasilios Magriplis <etameson@gmail.com>
2020-12-10 23:08:08 +1100

    Minor changes

--- src/Boring32.UnitTests/Strings/Strings.cpp
+++ src/Boring32.UnitTests/Strings/Strings.cpp
@@ -9,11 +9,18 @@ namespace Strings
 	TEST_CLASS(Strings)
 	{
 		public:
-			TEST_METHOD(TestTokeniseString)
+			TEST_METHOD(TestTokeniseString1)
 			{
 				std::wstring test = L"Hello,World,";
 				std::vector<std::wstring> strings = Boring32::Strings::TokeniseString(test, L",");
 				Assert::IsTrue(strings.size() == 3);
+			}
+
+			TEST_METHOD(TestTokeniseString2)
+			{
+				std::wstring test = L"";
+				std::vector<std::wstring> strings = Boring32::Strings::TokeniseString(test, L",");
+				Assert::IsTrue(strings.size() == 1);
 			}
 	};
 }
