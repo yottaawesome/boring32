4277467145385b8be5d12260269deed8f27a8d52
Vasilios Magriplis <etameson@gmail.com>
2021-01-21 21:04:09 +1100

    Add Util::ToByteVector() and new unit test

---
+++ src/Boring32.UnitTests/Util/Util.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "Boring32/include/Util/Util.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Util
+{
+	TEST_CLASS(Util)
+	{
+		public:
+			TEST_METHOD(TestToByteVector)
+			{
+				std::vector<std::byte> bytes = Boring32::Util::ToByteVector(0x0, 0x1, 0x2, 0x3);
+				Assert::IsTrue(bytes.size() == 4);
+				for(int i = 0; i < bytes.size(); i++)
+					Assert::IsTrue(bytes[i] == (std::byte)i);
+			}
+	};
+}
--- src/Boring32/include/Util/Util.hpp
+++ src/Boring32/include/Util/Util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <stdexcept>
+#include <vector>
 #include <Windows.h>
 
 #define colconc(str1,str2) #str1 ": " #str2
@@ -11,4 +12,10 @@ namespace Boring32::Util
 	SYSTEMTIME LargeIntegerTimeToSystemTime(const LARGE_INTEGER& li);
 	size_t GetUnixTime();
 	DWORD GetMillisToMinuteBoundary(const SYSTEMTIME& time, const UINT boundary);
+	// based on https://stackoverflow.com/questions/45172052/correct-way-to-initialize-a-container-of-stdbyte
+	template<typename... Ts>
+	std::vector<std::byte> ToByteVector(Ts&&... args) noexcept 
+	{
+		return{ std::byte(std::forward<Ts>(args))... };
+	}
 }
