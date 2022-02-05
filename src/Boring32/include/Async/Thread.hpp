966aec40af6bec5edd544804815655fce816fa0f
Vasilios Magriplis <etameson@gmail.com>
2022-02-05 11:54:10 +1100

    Merge ThreadStatus.hpp into Thread.hpp

--- src/Boring32/include/Async/Thread.hpp
+++ src/Boring32/include/Async/Thread.hpp
@@ -1,13 +1,21 @@
 #pragma once
 #include <Windows.h>
 #include <functional>
-#include "ThreadStatus.hpp"
 
 import boring32.raii.win32handle;
 import boring32.async.event;
 
 namespace Boring32::Async
 {
+	enum class ThreadStatus
+	{
+		Finished = 0,
+		Ready = 1,
+		Running = 2,
+		Suspended = 3,
+		Terminated = 4
+	};
+
 	class Thread
 	{
 		// Default constructible, movable, not copyable
