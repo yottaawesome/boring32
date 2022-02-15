38b8d951eb92e519f6e75f1e4b69eb4892580823
Vasilios Magriplis <etameson@gmail.com>
2022-02-15 20:16:48 +1100

    Continue SourceVoice

--- src/Boring32/XAudio2/boring32.xaudio2.sourcevoice.cpp
+++ src/Boring32/XAudio2/boring32.xaudio2.sourcevoice.cpp
@@ -1,6 +1,35 @@
+module;
+
+#include <stdexcept>
+#include <source_location>
+#include <Windows.h>
+#include <xaudio2.h>
+
 module boring32.xaudio2.sourcevoice;
+import boring32.error.errorbase;
 
 namespace Boring32::XAudio2
 {
+	SourceVoice::~SourceVoice()
+	{
+		Close();
+	}
 
+	SourceVoice::SourceVoice(IXAudio2SourceVoice* voice)
+		: m_voice(voice)
+	{
+		if (!voice) throw Error::ErrorBase<std::runtime_error>(
+			std::source_location::current(),
+			"voice cannot be nullptr"
+		);
+	}
+
+	void SourceVoice::Close()
+	{
+		if (m_voice)
+		{
+			m_voice->DestroyVoice();
+			m_voice = nullptr;
+		}
+	}
 }
--- src/Boring32/XAudio2/boring32.xaudio2.sourcevoice.ixx
+++ src/Boring32/XAudio2/boring32.xaudio2.sourcevoice.ixx
@@ -1,10 +1,24 @@
+module;
+
+#include <Windows.h>
+#include <xaudio2.h>
+
 export module boring32.xaudio2.sourcevoice;
 import boring32.xaudio2.voice;
 
 export namespace Boring32::XAudio2
 {
+	// https://docs.microsoft.com/en-us/windows/win32/api/xaudio2/nn-xaudio2-ixaudio2sourcevoice
 	class SourceVoice : public Voice
 	{
+		public:
+			virtual ~SourceVoice();
+			SourceVoice(IXAudio2SourceVoice* voice);
 
+		public:
+			virtual void Close();
+
+		protected:
+			IXAudio2SourceVoice* m_voice;
 	};
 }
