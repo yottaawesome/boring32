f58de404955a2a1a26f1590da5f0c8d99ede6651
Vasilios Magriplis <etameson@gmail.com>
2022-03-09 21:12:37 +1100

    Test: add TestDecompressorGetDecompressedSizeXPRESS()

--- src/Boring32.UnitTests/Compression/Decompressor.cpp
+++ src/Boring32.UnitTests/Compression/Decompressor.cpp
@@ -46,5 +46,22 @@ namespace Compression
 
 				Assert::IsTrue(decompressor.GetDecompressedSize(compressedData) > 0);
 			}
+
+			TEST_METHOD(TestDecompressorGetDecompressedSizeXPRESS)
+			{
+				const auto type = Boring32::Compression::CompressionType::XPRESS;
+
+				Boring32::Compression::Compressor compressor(type);
+				const std::byte* buffer = (std::byte*)&m_compressionString[0];
+				const auto compressedData = compressor.CompressBuffer({
+						buffer,
+						buffer + m_compressionString.size()
+					}
+				);
+
+				Boring32::Compression::Decompressor decompressor(type);
+
+				Assert::IsTrue(decompressor.GetDecompressedSize(compressedData) > 0);
+			}
 	};
 }
