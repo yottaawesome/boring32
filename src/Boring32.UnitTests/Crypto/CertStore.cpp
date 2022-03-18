979ce4c046837b7ddd049055953f145143e607e7
Vasilios Magriplis <etameson@gmail.com>
2022-03-18 20:14:28 +1100

    Test CertStore move constructor

--- src/Boring32.UnitTests/Crypto/CertStore.cpp
+++ src/Boring32.UnitTests/Crypto/CertStore.cpp
@@ -44,6 +44,17 @@ namespace Crypto
 				);
 			}
 
+			TEST_METHOD(TestMoveConstructor)
+			{
+				Boring32::Crypto::CertStore store1(L"MY");
+				Boring32::Crypto::CertStore store2(std::move(store1));
+				Assert::IsNull(store1.GetHandle());
+				Assert::IsNotNull(store2.GetHandle());
+				Assert::IsTrue(
+					Boring32::Crypto::CertStoreType::CurrentUser == store2.GetStoreType()
+				);
+			}
+
 			TEST_METHOD(TestCreateUserCertStore)
 			{
 				Boring32::Crypto::CertStore store(L"MY");
