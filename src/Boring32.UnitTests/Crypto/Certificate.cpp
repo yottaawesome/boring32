d0f66429d0767ad3eb92a268d3dca549d4c6adbb
Vasilios Magriplis <etameson@gmail.com>
2022-03-21 19:25:02 +1100

    Add Certificate load and copy assignment tests

--- src/Boring32.UnitTests/Crypto/Certificate.cpp
+++ src/Boring32.UnitTests/Crypto/Certificate.cpp
@@ -16,12 +16,29 @@ namespace Crypto
 				Assert::IsNull(cert.GetCert());
 			}
 
+			TEST_METHOD(TestCertLoad)
+			{
+				Boring32::Crypto::CertStore certStore(L"MY");
+				Boring32::Crypto::Certificate cert
+					= certStore.GetCertBySubjectCn(L"client.localhost");
+				Assert::IsNotNull(cert.GetCert());
+			}
+
 			TEST_METHOD(TestGetSignatureHashCngAlgorithm)
 			{
 				Boring32::Crypto::CertStore certStore(L"MY");
-				Boring32::Crypto::Certificate cert 
+				Boring32::Crypto::Certificate cert
 					= certStore.GetCertBySubjectCn(L"client.localhost");
 				Assert::IsTrue(cert.GetSignatureHashCngAlgorithm() == L"RSA/SHA256");
+			}
+
+			TEST_METHOD(TestCopyAssignment)
+			{
+				Boring32::Crypto::CertStore certStore(L"MY");
+				Boring32::Crypto::Certificate cert1 = certStore.GetCertBySubjectCn(L"client.localhost");
+				Boring32::Crypto::Certificate cert2 = cert1;
+				Assert::IsNotNull(cert1.GetCert());
+				Assert::IsNotNull(cert2.GetCert());
 			}
 	};
 }
