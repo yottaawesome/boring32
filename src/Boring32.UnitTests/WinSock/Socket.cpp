057deba535407beeb5b63e63ad82d4bca88a7679
Vasilios Magriplis <etameson@gmail.com>
2022-03-24 18:04:26 +1100

    Add test for Socket default constructor

---
+++ src/Boring32.UnitTests/WinSock/Socket.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include <stdexcept>
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+import boring32.winsock;
+import boring32.winsock.winsockerror;
+
+namespace WinSock
+{
+	TEST_CLASS(Socket)
+	{
+		public:
+			TEST_METHOD(TestDefaultConstructor)
+			{
+				Boring32::WinSock::WinSockInit init(2,2);
+				Boring32::WinSock::Socket socket;
+			}
+	};
+}
