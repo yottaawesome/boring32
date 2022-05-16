22279fbe5b2383733297152ed9549a43639c9f36
Vasilios Magriplis <etameson@gmail.com>
2022-05-16 19:59:38 +1000

    Add TestDecompressorDecompressLZMS()

--- src/Boring32.UnitTests/Compression/Decompressor.cpp
+++ src/Boring32.UnitTests/Compression/Decompressor.cpp
@@ -88,6 +88,19 @@ namespace Compression
 				Assert::IsTrue(decompressedString == m_compressionString);
 			}
 
+			TEST_METHOD(TestDecompressorDecompressLZMS)
+			{
+				const auto type = Boring32::Compression::CompressionType::LZMS;
+				Boring32::Compression::Compressor compressor(type);
+				const std::byte* buffer = (std::byte*)&m_compressionString[0];
+				std::vector<std::byte> compressedData = compressor.CompressBuffer({ buffer, buffer + m_compressionString.size() });
+
+				Boring32::Compression::Decompressor decompressor(type);
+				std::vector<std::byte> decompressedData = decompressor.DecompressBuffer(compressedData);
+				std::string decompressedString(reinterpret_cast<char*>(&decompressedData[0]), decompressedData.size());
+				Assert::IsTrue(decompressedString == m_compressionString);
+			}
+
 			TEST_METHOD(TestDecompressorGetDecompressedSizeMSZIP)
 			{
 				const auto type = Boring32::Compression::CompressionType::MSZIP;
