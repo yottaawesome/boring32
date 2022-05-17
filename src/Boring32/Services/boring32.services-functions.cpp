173eacfca884d2d8d4ad326c4cbee8fb33e0be0f
Vasilios Magriplis <etameson@gmail.com>
2022-05-17 18:37:27 +1000

    Add OpenServiceHandle()

--- src/Boring32/Services/boring32.services-functions.cpp
+++ src/Boring32/Services/boring32.services-functions.cpp
@@ -12,7 +12,7 @@ namespace Boring32::Services
 	SC_HANDLE OpenServiceControlManager(const DWORD desiredAccess)
 	{
 		// https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-openscmanagerw
-		SC_HANDLE handle = OpenSCManagerW(
+		const SC_HANDLE handle = OpenSCManagerW(
 			nullptr,
 			SERVICES_ACTIVE_DATABASE,
 			desiredAccess // https://docs.microsoft.com/en-us/windows/win32/services/service-security-and-access-rights
@@ -29,5 +29,29 @@ namespace Boring32::Services
 	SERVICE_STATUS_PROCESS GetServiceStatus()
 	{
 		return { 0 };
+	}
+
+	SC_HANDLE OpenServiceHandle(
+		const SC_HANDLE scmHandle,
+		const std::wstring& serviceName,
+		const DWORD desiredAccess
+	)
+	{
+		if (!scmHandle)
+			throw Boring32::Error::Boring32Error(std::source_location::current(), "SCM handle cannot be null");
+
+		// https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-openservicew
+		const SC_HANDLE serviceHandle = OpenServiceW(
+			scmHandle,
+			serviceName.c_str(),
+			desiredAccess
+		);
+		if (!serviceHandle)
+		{
+			const auto lastError = GetLastError();
+			throw Error::Win32Error(std::source_location::current(), "OpenServiceW() failed", lastError);
+		}
+
+		return serviceHandle;
 	}
 }
--- src/Boring32/Services/boring32.services-functions.ixx
+++ src/Boring32/Services/boring32.services-functions.ixx
@@ -1,5 +1,6 @@
 module;
 
+#include <string>
 #include <Windows.h>
 
 export module boring32.services:functions;
@@ -8,4 +9,9 @@ export namespace Boring32::Services
 {
 	[[nodiscard]] SC_HANDLE OpenServiceControlManager(const DWORD desiredAccess);
 	[[nodiscard]] SERVICE_STATUS_PROCESS GetServiceStatus();
+	[[nodiscard]] SC_HANDLE OpenServiceHandle(
+		const SC_HANDLE scmHandle, 
+		const std::wstring& serviceName, 
+		const DWORD desiredAccess
+	);
 }
