1707f8f3be31e01716e7813cc6e3379d6140d626
Vasilios Magriplis <etameson@gmail.com>
2022-05-22 10:17:33 +1000

    Minor changes

--- src/Boring32.UnitTests/WinHttp/Session.cpp
+++ src/Boring32.UnitTests/WinHttp/Session.cpp
@@ -10,6 +10,8 @@ namespace WinHttp
 	TEST_CLASS(Session)
 	{
 		const std::wstring UserAgent = L"TestUserAgent";
+		// I don't have an available proxy to test the other types...
+		const Boring32::WinHttp::ProxyType ProxyType = Boring32::WinHttp::ProxyType::NoProxy;
 
 		public:
 			TEST_METHOD(TestSessionDefaultConstructor)
@@ -26,9 +28,9 @@ namespace WinHttp
 
 			TEST_METHOD(TestSessionConstructorUserAgentProxyType)
 			{
-				Boring32::WinHttp::Session session(UserAgent, Boring32::WinHttp::ProxyType::NoProxy);
+				Boring32::WinHttp::Session session(UserAgent, ProxyType);
 				Assert::IsTrue(session.GetUserAgent() == UserAgent);
-				Assert::IsTrue(session.GetProxyType() == Boring32::WinHttp::ProxyType::NoProxy);
+				Assert::IsTrue(session.GetProxyType() == ProxyType);
 				Assert::IsNotNull(session.GetSession());
 			}
 	};
