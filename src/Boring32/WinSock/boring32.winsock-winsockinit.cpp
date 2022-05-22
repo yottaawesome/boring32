541fa80bfc90a412f1b1d03a43c3ef401475e5bb
Vasilios Magriplis <etameson@gmail.com>
2022-05-22 12:08:38 +1000

    Improve WinSockInit to support copy semantics

--- src/Boring32/WinSock/boring32.winsock-winsockinit.cpp
+++ src/Boring32/WinSock/boring32.winsock-winsockinit.cpp
@@ -13,32 +13,60 @@ namespace Boring32::WinSock
 {
 	WinSockInit::~WinSockInit()
 	{
-		WSACleanup();
+		Close();
 	}
 	
-	WinSockInit::WinSockInit()
+	WinSockInit::WinSockInit() : m_highVersion(2), m_lowVersion(2)
 	{
-		Initialize(2, 2);
+		Initialize();
+	}
+
+	WinSockInit::WinSockInit(const WinSockInit& other)
+	{
+		Copy(other);
 	}
 
 	WinSockInit::WinSockInit(const DWORD highVersion, const DWORD lowVersion)
+		: m_highVersion(highVersion), m_lowVersion(lowVersion)
 	{
-		Initialize(highVersion, lowVersion);
+		Initialize();
 	}
 
-	void WinSockInit::Initialize(const DWORD highVersion, const DWORD lowVersion)
+	void WinSockInit::Initialize()
 	{
 		//https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsastartup
-		if (const int err = WSAStartup(MAKEWORD(highVersion, lowVersion), &m_wsaData))
-			Error::ThrowNested(
-				Error::Win32Error(std::source_location::current(), "WSAStartup() failed", err, L"Ws2_32.dll"),
-				WinSockError(std::source_location::current(), "Failed to initialise WinSock")
-			);
+		const int error = WSAStartup(MAKEWORD(m_highVersion, m_lowVersion), &m_wsaData);
+		if (error) Error::ThrowNested(
+			Error::Win32Error(std::source_location::current(), "WSAStartup() failed", error, L"Ws2_32.dll"),
+			WinSockError(std::source_location::current(), "Failed to initialise WinSock")
+		);
 	}
 
 	const WSAData& WinSockInit::GetData() const noexcept
 	{
 		//https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
 		return m_wsaData;
+	}
+	
+	void WinSockInit::Close()
+	{
+		WSACleanup();
+		m_lowVersion = 0;
+		m_highVersion = 0;
+	}
+
+	WinSockInit& WinSockInit::Copy(const WinSockInit& other)
+	{
+		if (&other == this)
+			return *this;
+		Close();
+		if (other.m_highVersion && other.m_lowVersion)
+		{
+			m_highVersion = other.m_highVersion;
+			m_lowVersion = other.m_lowVersion;
+			Initialize();
+		}
+
+		return *this;
 	}
 }
--- src/Boring32/WinSock/boring32.winsock-winsockinit.ixx
+++ src/Boring32/WinSock/boring32.winsock-winsockinit.ixx
@@ -12,15 +12,20 @@ export namespace Boring32::WinSock
 		public:
 			virtual ~WinSockInit();
 			WinSockInit();
+			WinSockInit(const WinSockInit& other);
 			WinSockInit(const DWORD highVersion, const DWORD lowVersion);
 
 		public:
 			const WSAData& GetData() const noexcept;
+			virtual void Close();
 
 		protected:
-			virtual void Initialize(const DWORD highVersion, const DWORD lowVersion);
+			virtual void Initialize();
+			virtual WinSockInit& Copy(const WinSockInit& other);
 
 		protected:
 			WSAData m_wsaData;
+			DWORD m_lowVersion;
+			DWORD m_highVersion;
 	};
 }
