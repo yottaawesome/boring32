f4d816d637d97930fdac44a445676dec29ac3ca6
Vasilios Magriplis <etameson@gmail.com>
2022-05-22 10:10:42 +1000

    More tests

--- src/Boring32.UnitTests/WinHttp/Session.cpp
+++ src/Boring32.UnitTests/WinHttp/Session.cpp
@@ -5,14 +5,22 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 import boring32.winhttp;
 
-namespace Tests::WinHttp
+namespace WinHttp
 {
 	TEST_CLASS(Session)
 	{
+		const std::wstring UserAgent = L"TestUserAgent";
+
 		public:
 			TEST_METHOD(TestSessionDefaultConstructor)
 			{
 				Boring32::WinHttp::Session session;
+			}
+
+			TEST_METHOD(TestSessionConstructorUserAgent)
+			{
+				Boring32::WinHttp::Session session(UserAgent);
+				Assert::IsTrue(session.GetUserAgent() == UserAgent);
 			}
 	};
 }
