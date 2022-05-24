0c43f65ce7b33f381132d53fbc0b92f767a50a47
Vasilios Magriplis <etameson@gmail.com>
2022-05-24 19:15:33 +1000

    Minor cleanup in MasteringVoice

--- src/Boring32/XAudio2/boring32.xaudio2-xaudio2error.cpp
+++ src/Boring32/XAudio2/boring32.xaudio2-xaudio2error.cpp
@@ -9,25 +9,9 @@ import boring32.error;
 namespace Boring32::XAudio2
 {
 	XAudio2Error::~XAudio2Error() = default;
-
-	XAudio2Error::XAudio2Error(const XAudio2Error&) = default;
-	XAudio2Error::XAudio2Error(XAudio2Error&&) noexcept = default;
-	XAudio2Error& XAudio2Error::operator=(const XAudio2Error&) = default;
-	XAudio2Error& XAudio2Error::operator=(XAudio2Error&&) noexcept = default;
-
 	XAudio2Error::XAudio2Error(
 		const std::source_location& location,
 		const std::string& message
 	) : Error::Boring32Error(location, message)
-	{
-		GenerateErrorMessage(location, message);
-	}
-
-	void XAudio2Error::GenerateErrorMessage(
-		const std::source_location& location,
-		const std::string& message
-	)
-	{
-		m_message = Error::FormatErrorMessage("XAudio2", location, message);
-	}
+	{ }
 }
