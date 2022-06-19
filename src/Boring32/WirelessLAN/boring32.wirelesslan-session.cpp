9070b7ee58e9a8a23e9151edc87e7595a9fa8f56
Vasilios Magriplis <etameson@gmail.com>
2022-06-19 10:32:37 +1000

    Add first cut of Session

--- src/Boring32/WirelessLAN/boring32.wirelesslan-session.cpp
+++ src/Boring32/WirelessLAN/boring32.wirelesslan-session.cpp
@@ -1,6 +1,38 @@
+module;
+
+#include <format>
+#include <iostream>
+#include <source_location>
+#include <Windows.h>
+#include <wlanapi.h>
+#include <wtypes.h>
+
 module boring32.wirelesslan:session;
+import boring32.error;
 
 namespace Boring32::WirelessLAN
 {
 	Session::~Session() {}
+
+	Session::Session()
+        : m_maxClientVersion(2),
+        m_negotiatedVersion(0)
+	{
+        Open();
+	}
+
+    void Session::Open()
+    {
+        // Open a WLAN session
+        HANDLE wlanHandle;
+        DWORD status = WlanOpenHandle(
+            m_maxClientVersion,
+            nullptr,
+            &m_negotiatedVersion,
+            &wlanHandle
+        );
+        if (status != ERROR_SUCCESS)
+            throw Boring32::Error::Win32Error("WlanOpenHandle() failed", status);
+        m_wlanHandle = CreateSharedWLANHandle(wlanHandle);
+    }
 }
--- src/Boring32/WirelessLAN/boring32.wirelesslan-session.ixx
+++ src/Boring32/WirelessLAN/boring32.wirelesslan-session.ixx
@@ -1,3 +1,7 @@
+module;
+
+#include <Windows.h>
+
 export module boring32.wirelesslan:session;
 import :cleanup;
 
@@ -6,9 +10,15 @@ export namespace Boring32::WirelessLAN
 	class Session
 	{
 		public:
-			virtual ~Session();
+			virtual ~Session(); 
+			Session();
 
 		protected:
+			virtual void Open();
 
+		protected:
+			SharedWLANHandle m_wlanHandle;
+			DWORD m_maxClientVersion;
+			DWORD m_negotiatedVersion;
 	};
 }
