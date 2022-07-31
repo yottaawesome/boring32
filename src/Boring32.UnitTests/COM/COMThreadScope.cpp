d3f292dabe951a76e2c957fedf562af0d14e1019
Vasilios Magriplis <etameson@gmail.com>
2022-07-31 11:55:08 +1000

    Add UnitTests::COMThreadScope

---
+++ src/Boring32.UnitTests/COM/COMThreadScope.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+import boring32.com;
+
+namespace COM
+{
+	TEST_CLASS(COMThreadScope)
+	{
+		public:
+			TEST_METHOD(TestCompressorDefaultConstructor)
+			{
+				Boring32::COM::COMThreadScope threadScope;
+			}
+	};
+}
