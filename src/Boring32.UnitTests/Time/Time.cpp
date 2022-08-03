fa7f5493ff06f3f2d70247f00922191cbb192da6
Vasilios Magriplis <etameson@gmail.com>
2022-08-04 07:39:56 +1000

    Add Time unit test

---
+++ src/Boring32.UnitTests/Time/Time.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <vector>
+#include <string>
+#include "CppUnitTest.h"
+
+import boring32.time;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Time
+{
+	TEST_CLASS(Time)
+	{
+		public:
+			TEST_METHOD(TestAddSeconds)
+			{
+				SYSTEMTIME st{
+					.wYear = 2022,
+					.wMonth = 8,
+					.wDayOfWeek = 4,
+					.wDay = 4,
+					.wHour = 13,
+					.wMinute = 25,
+					.wSecond = 30,
+					.wMilliseconds = 500
+				};
+				Boring32::Time::DateTime dt(st);
+
+				dt.AddSeconds(1);
+				st = dt.ToSystemTime();
+				Assert::IsTrue(st.wSecond == 31);
+			}
+	};
+}
