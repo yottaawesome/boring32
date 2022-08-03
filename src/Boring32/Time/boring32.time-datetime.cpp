04509fe90da87ff62d778bebbff0302da7a44e37
Vasilios Magriplis <etameson@gmail.com>
2022-08-03 18:45:31 +1000

    Continue implementing DateTime

--- src/Boring32/Time/boring32.time-datetime.cpp
+++ src/Boring32/Time/boring32.time-datetime.cpp
@@ -1,6 +1,22 @@
+module;
+
+#include <cstdint>
+#include <Windows.h>
+
 module boring32.time:datetime;
+import :functions;
 
 namespace Boring32::Time
 {
+	DateTime::~DateTime() {};
 
+	DateTime::DateTime()
+	{
+		GetSystemTimeAsFileTime(&m_ft);
+	}
+
+	uint64_t DateTime::ToMicroSeconds() const noexcept
+	{
+		return FromFileTime(m_ft) * 10ull;
+	}
 }
--- src/Boring32/Time/boring32.time-datetime.ixx
+++ src/Boring32/Time/boring32.time-datetime.ixx
@@ -1,6 +1,22 @@
+module;
+
+#include <cstdint>
+#include <Windows.h>
+
 export module boring32.time:datetime;
 
 export namespace Boring32::Time
 {
+	class DateTime
+	{
+		public:
+			virtual ~DateTime();
+			DateTime();
 
+		public:
+			virtual uint64_t ToMicroSeconds() const noexcept;
+
+		protected:
+			FILETIME m_ft{ 0 };
+	};
 }
