4923a3bdebbe81016155e2ff1668440afc0a81e1
Vasilios Magriplis <etameson@gmail.com>
2022-08-06 12:59:44 +1000

    Add TestSubtractSeconds

--- src/Boring32.UnitTests/Time/DateTime.cpp
+++ src/Boring32.UnitTests/Time/DateTime.cpp
@@ -27,5 +27,22 @@ namespace Time
 				dt.AddSeconds(1);
 				Assert::IsTrue(dt.ToSystemTime().wSecond == 31);
 			}
+
+			TEST_METHOD(TestSubtractSeconds)
+			{
+				const SYSTEMTIME st{
+					.wYear = 2022,
+					.wMonth = 8,
+					.wDayOfWeek = 4,
+					.wDay = 4,
+					.wHour = 13,
+					.wMinute = 25,
+					.wSecond = 30,
+					.wMilliseconds = 500
+				};
+				Boring32::Time::DateTime dt(st);
+				dt.AddSeconds(-1);
+				Assert::IsTrue(dt.ToSystemTime().wSecond == 29);
+			}
 	};
 }
