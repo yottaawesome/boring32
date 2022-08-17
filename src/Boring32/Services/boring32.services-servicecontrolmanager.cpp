9875c16f4e95e84df34297fd1c45cdd5ac0c72c1
Vasilios Magriplis <etameson@gmail.com>
2022-08-17 19:06:57 +1000

    Continue ServiceControlManager

--- src/Boring32/Services/boring32.services-servicecontrolmanager.cpp
+++ src/Boring32/Services/boring32.services-servicecontrolmanager.cpp
@@ -1,6 +1,41 @@
+module;
+
+#include <source_location>
+#include <Windows.h>
+
 module boring32.services:servicecontrolmanager;
+import boring32.error;
 
 namespace Boring32::Services
 {
-	
+	ServiceControlManager::~ServiceControlManager(){}
+
+	ServiceControlManager::ServiceControlManager()
+	{
+		Open();
+	}
+
+	void ServiceControlManager::Open()
+	{
+        SC_HANDLE schSCManager;
+
+        // Get a handle to the SCM database. 
+
+        schSCManager = OpenSCManagerW(
+            nullptr,                // local computer
+            nullptr,                // ServicesActive database 
+            SC_MANAGER_ALL_ACCESS   // full access rights
+        );  
+        if (!schSCManager)
+        {
+            const auto lastError = GetLastError();
+            throw Error::Win32Error("OpenSCManagerW() failed", lastError);
+        }
+        m_scm = CreateSharedPtr(schSCManager);
+	}
+
+    void ServiceControlManager::Close()
+    {
+        m_scm = nullptr;
+    }
 }
--- src/Boring32/Services/boring32.services-servicecontrolmanager.ixx
+++ src/Boring32/Services/boring32.services-servicecontrolmanager.ixx
@@ -1,9 +1,19 @@
 export module boring32.services:servicecontrolmanager;
+import :raii;
 
 export namespace Boring32::Services
 {
 	class ServiceControlManager
 	{
+		public:
+			virtual ~ServiceControlManager();
+			ServiceControlManager();
 
+		protected:
+			virtual void Open();
+			virtual void Close();
+
+		protected:
+			ServiceHandleSharedPtr m_scm;
 	};
 }
