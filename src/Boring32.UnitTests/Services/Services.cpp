c5669ba73de9d41047816839e83af72a109d68f8
Vasilios Magriplis <etameson@gmail.com>
2022-08-17 19:22:34 +1000

    Add test for ServiceControlManager

---
+++ src/Boring32.UnitTests/Services/Services.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <vector>
+#include <string>
+#include "CppUnitTest.h"
+
+import boring32.services;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Services
+{
+	TEST_CLASS(ServiceControlManager)
+	{
+		public:
+			TEST_METHOD(TestTokeniseString1)
+			{
+				Boring32::Services::ServiceControlManager scm(SC_MANAGER_CONNECT);
+			}
+	};
+}
