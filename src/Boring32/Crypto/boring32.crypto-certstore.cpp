af8ffed636cd8f738f212d8e1f278498577bdc05
Vasilios Magriplis <etameson@gmail.com>
2023-04-23 14:35:53 +1000

    Remove <source_location>

--- src/Boring32/Crypto/boring32.crypto-certstore.cpp
+++ src/Boring32/Crypto/boring32.crypto-certstore.cpp
@@ -1,6 +1,3 @@
-module;
-
-#include <source_location>
 //#include <Windows.h>
 //#include <dpapi.h> // not including this header causes symbol has already been defined error
 //#include <wincrypt.h>
