95c0e4e7ba6573ab762040692c80f52f0385791c
Vasilios Magriplis <etameson@gmail.com>
2023-05-07 18:04:14 +1000

    Add TestCopyConstructor

--- src/Boring32.UnitTests/Error/boring32.error-comerror.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-comerror.cpp
@@ -18,5 +18,13 @@ namespace Error
 				Assert::IsTrue(error.GetHResult() == m_errorCode);
 				Assert::IsTrue(std::string(error.what()).contains(m_errorMessage));
 			}
+
+			TEST_METHOD(TestCopyConstructor)
+			{
+				Boring32::Error::COMError error1(m_errorMessage, m_errorCode);
+				Boring32::Error::COMError error2(error1);
+				Assert::IsTrue(error1.GetHResult() == error2.GetHResult());
+				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
+			}
 	};
 }
