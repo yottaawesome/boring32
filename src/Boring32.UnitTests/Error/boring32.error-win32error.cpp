6938899d1a3f015475350a0cb552f6644274275f
Vasilios Magriplis <etameson@gmail.com>
2023-05-07 11:22:09 +1000

    Add TestCopyAssignment

--- src/Boring32.UnitTests/Error/boring32.error-win32error.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-win32error.cpp
@@ -24,5 +24,13 @@ namespace Error
 				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
 				Assert::IsTrue(error1.GetErrorCode() == error2.GetErrorCode());
 			}
+
+			TEST_METHOD(TestCopyAssignment)
+			{
+				Boring32::Error::Win32Error error1(m_errorMessage, 0x5);
+				Boring32::Error::Win32Error error2 = error1;
+				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
+				Assert::IsTrue(error1.GetErrorCode() == error2.GetErrorCode());
+			}
 	};
 }
