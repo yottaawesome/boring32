a068b51a3de2e5d5ab80638be75102674fd56364
Vasilios Magriplis <etameson@gmail.com>
2023-05-08 17:07:02 +1000

    Add TestCopyAssignment

--- src/Boring32.UnitTests/Error/boring32.error-ntstatuserror.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-ntstatuserror.cpp
@@ -38,5 +38,13 @@ namespace Error
 				Assert::IsTrue(error1.GetErrorCode() == error2.GetErrorCode());
 				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
 			}
+
+			TEST_METHOD(TestCopyAssignment)
+			{
+				Boring32::Error::NTStatusError error1(m_errorMessage, m_errorCode);
+				Boring32::Error::NTStatusError error2 = error1;
+				Assert::IsTrue(error1.GetErrorCode() == error2.GetErrorCode());
+				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
+			}
 	};
 }
