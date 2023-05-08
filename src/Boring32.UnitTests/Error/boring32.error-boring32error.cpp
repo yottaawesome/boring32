1ca6f10cb89bbb5cd539f75ec5a191a4e72ed0cd
Vasilios Magriplis <etameson@gmail.com>
2023-05-08 17:19:59 +1000

    Add TestMoveConstructor

--- src/Boring32.UnitTests/Error/boring32.error-boring32error.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-boring32error.cpp
@@ -36,5 +36,13 @@ namespace Error
 				Boring32::Error::Boring32Error error2 = error1;
 				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
 			}
+
+			TEST_METHOD(TestMoveConstructor)
+			{
+				Boring32::Error::Boring32Error error1(m_errorMessage);
+				Boring32::Error::Boring32Error error2(std::move(error1));
+				Assert::IsTrue(std::string(error1.what()).empty());
+				Assert::IsTrue(std::string(error2.what()).contains(m_errorMessage));
+			}
 	};
 }
