ead52724a855f1ad2c1865c89bcad4d51f9f4ac1
Vasilios Magriplis <etameson@gmail.com>
2023-05-17 19:44:48 +1000

    Add TestMoveConstructor

--- src/Boring32.UnitTests/Error/boring32.error-errorbase.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-errorbase.cpp
@@ -28,5 +28,15 @@ namespace Error
 				Boring32::Error::ErrorBase<std::exception> error2 = error1;
 				Assert::IsTrue(std::string(error1.what()) == std::string(error2.what()));
 			}
+
+			TEST_METHOD(TestMoveConstructor)
+			{
+				Boring32::Error::ErrorBase<std::exception> error1("Oh no");
+				Boring32::Error::ErrorBase<std::exception> error2(std::move(error1));
+				Assert::IsTrue(std::string(error1.what()).empty());
+				Assert::IsFalse(std::string(error2.what()).empty());
+				Assert::IsTrue(error1.GetStacktrace().empty());
+				Assert::IsFalse(error2.GetStacktrace().empty());
+			}
 	};
 }
