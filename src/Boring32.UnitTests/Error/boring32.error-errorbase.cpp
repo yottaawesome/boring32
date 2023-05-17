c81d45542ff83a2872fa2dd4ba5fad95aac8b1cf
Vasilios Magriplis <etameson@gmail.com>
2023-05-17 19:45:36 +1000

    Add TestMoveAssignment

--- src/Boring32.UnitTests/Error/boring32.error-errorbase.cpp
+++ src/Boring32.UnitTests/Error/boring32.error-errorbase.cpp
@@ -38,5 +38,15 @@ namespace Error
 				Assert::IsTrue(error1.GetStacktrace().empty());
 				Assert::IsFalse(error2.GetStacktrace().empty());
 			}
+
+			TEST_METHOD(TestMoveAssignment)
+			{
+				Boring32::Error::ErrorBase<std::exception> error1("Oh no");
+				Boring32::Error::ErrorBase<std::exception> error2 = std::move(error1);
+				Assert::IsTrue(std::string(error1.what()).empty());
+				Assert::IsFalse(std::string(error2.what()).empty());
+				Assert::IsTrue(error1.GetStacktrace().empty());
+				Assert::IsFalse(error2.GetStacktrace().empty());
+			}
 	};
 }
