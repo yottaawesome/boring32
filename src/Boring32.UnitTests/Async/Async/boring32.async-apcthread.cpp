434edb8b572037d7934d2d0592d0c250f82f4887
Vasilios Magriplis <etameson@gmail.com>
2023-06-11 08:44:02 +1000

    Add some tests for APCThread

---
+++ src/Boring32.UnitTests/Async/Async/boring32.async-apcthread.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+import boring32.async;
+
+namespace Async
+{
+	TEST_CLASS(APCThread)
+	{
+		TEST_METHOD(TestQueueAPC)
+		{
+			Boring32::Async::APCThread apcExecutor;
+			apcExecutor.Start();
+			Assert::IsTrue(apcExecutor.WaitToStart(1000));
+			bool test = false;
+			apcExecutor.QueueAPC
+			(
+				[](ULONG_PTR arg)
+				{ 
+					bool& test = *reinterpret_cast<bool*>(arg);
+					test = true;
+				},
+				reinterpret_cast<ULONG_PTR>(&test)
+			);
+			apcExecutor.SignalToExit();
+			Assert::IsTrue(apcExecutor.Join(2500));
+			Assert::IsTrue(test);
+		}
+
+		TEST_METHOD(TestRun)
+		{
+			Boring32::Async::APCThread apcExecutor;
+			apcExecutor.Start();
+			Assert::IsTrue(apcExecutor.WaitToStart(1000));
+			apcExecutor.SignalToExit();
+			Assert::IsTrue(apcExecutor.Join(5000));
+			Assert::IsTrue(apcExecutor.GetExitCode() == 0);
+		}
+	};
+}
