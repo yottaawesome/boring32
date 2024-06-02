cc95b044ff2231ec442ec6ee64b5a442384235ac
Vasilios Magriplis <etameson@gmail.com>
2024-06-02 20:08:01 +1000

    Delete header unit.

--- src/Boring32.HeaderUnit/win32.cpp
+++
@@ -1,9 +0,0 @@
-// See https://learn.microsoft.com/en-us/cpp/build/walkthrough-header-units
-// https://learn.microsoft.com/en-us/cpp/build/walkthrough-import-stl-header-units
-#pragma warning(disable:4005)
-#pragma warning(disable:5106)
-// The above need to be disabled because of
-// https://developercommunity.visualstudio.com/t/warning-C4005:-Outptr:-macro-redefinit/1546919
-// No idea when this will get fixed, MS seems to be taking their time with it.
-import "win32.hpp";
-
