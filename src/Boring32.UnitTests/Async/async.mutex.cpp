deae5ee3be4e996b10b31ca99d33b098ca9ce0eb
Vasilios Magriplis <etameson@gmail.com>
2026-04-16 17:01:16 +1000

    Improve Mutex tests. Use AAA. Add test for Mutex::Close()

--- src/Boring32.UnitTests/Async/async.mutex.cpp
+++ src/Boring32.UnitTests/Async/async.mutex.cpp
@@ -19,21 +19,21 @@ namespace Async
 
 		TEST_METHOD(TestCreateAnonymousMutex)
 		{
-			Boring32::Async::Mutex testMutex(false, false);
+			auto testMutex = Boring32::Async::Mutex{ false, false };
 			Assert::IsNotNull(testMutex.GetHandle());
 		}
 
 		TEST_METHOD(TestCreateNamedMutex)
 		{
-			Boring32::Async::Mutex testMutex(false, false, L"Mutex1");
+			auto testMutex = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsNotNull(testMutex.GetHandle());
 			Assert::IsTrue(testMutex.GetName() == L"Mutex1");
 		}
 
 		TEST_METHOD(TestOpenNamedMutex)
 		{
-			Boring32::Async::Mutex testMutex1(false, false, L"Mutex1");
-			Boring32::Async::Mutex testMutex2(false, false, L"Mutex1", MUTEX_ALL_ACCESS);
+			auto testMutex1 = Boring32::Async::Mutex{ false, false, L"Mutex1" };
+			auto testMutex2 = Boring32::Async::Mutex{ false, false, L"Mutex1", MUTEX_ALL_ACCESS };
 			Assert::IsNotNull(testMutex1.GetHandle());
 			Assert::IsNotNull(testMutex2.GetHandle());
 			Assert::IsTrue(testMutex2.GetName() == L"Mutex1");
@@ -41,84 +41,83 @@ namespace Async
 
 		TEST_METHOD(TestMoveConstructor)
 		{
-			Boring32::Async::Mutex testMutex1(false, false, L"Mutex1");
-			Boring32::Async::Mutex testMutex2(std::move(testMutex1));
+			auto testMutex1 = Boring32::Async::Mutex{ false, false, L"Mutex1" };
+			auto testMutex2 = Boring32::Async::Mutex{ std::move(testMutex1) };
 			TestMove(testMutex1, testMutex2);
 		}
 
 		TEST_METHOD(TestMoveAssignment)
 		{
-			Boring32::Async::Mutex testMutex1(false, false, L"Mutex1");
-			Boring32::Async::Mutex testMutex2 = std::move(testMutex1);
+			auto testMutex1 = Boring32::Async::Mutex{ false, false, L"Mutex1" };
+			auto testMutex2 = Boring32::Async::Mutex{ };
+			testMutex2 = std::move(testMutex1);
 			TestMove(testMutex1, testMutex2);
 		}
 
 		TEST_METHOD(TestLock)
 		{
-			Boring32::Async::Mutex testMutex1(false, false, L"Mutex1");
+			auto testMutex1 = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsTrue(testMutex1.Lock(500, false));
 		}
 
 		TEST_METHOD(TestLockChrono)
 		{
-			Boring32::Async::Mutex testMutex1(false, false);
+			auto testMutex1 = Boring32::Async::Mutex{ false, false };
 			Assert::IsTrue(testMutex1.Lock(std::chrono::seconds(5), false));
 		}
 
 		TEST_METHOD(TestUnlock)
 		{
-			Boring32::Async::Mutex testMutex(false, false, L"Mutex1");
+			auto testMutex = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsTrue(testMutex.Lock(500, false));
 			testMutex.Unlock();
 		}
 
 		TEST_METHOD(TestGetName)
 		{
-			Boring32::Async::Mutex testMutex(false, false, L"Mutex1");
+			auto testMutex = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsTrue(testMutex.GetName() == L"Mutex1");
 		}
 
 		TEST_METHOD(TestLockException)
 		{
 			Assert::ExpectException<Boring32::Error::Boring32Error>(
-				[]()
-				{
-					Boring32::Async::Mutex testMutex;
+				[]{
+					auto testMutex = Boring32::Async::Mutex{};
 					testMutex.Lock(0, true);
 				});
 		}
 
 		TEST_METHOD(TestLockNoThrow)
 		{
-			Boring32::Async::Mutex testMutex;
+			auto testMutex = Boring32::Async::Mutex{};
 			Assert::IsFalse(testMutex.Lock(0, true, std::nothrow));
 		}
 
 		TEST_METHOD(TestTryUnlockWhenOwned)
 		{
-			Boring32::Async::Mutex testMutex(false, false, L"Mutex1");
+			auto testMutex = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsTrue(testMutex.Lock(500, false));
 			Assert::IsTrue(testMutex.TryUnlock());
 		}
 
 		TEST_METHOD(TestTryUnlockWhenNotOwned)
 		{
-			Boring32::Async::Mutex testMutex(false, false, L"Mutex1");
+			auto testMutex = Boring32::Async::Mutex{ false, false, L"Mutex1" };
 			Assert::IsFalse(testMutex.TryUnlock());
 		}
 
 		TEST_METHOD(TestTryUnlockOnNullMutex)
 		{
-			Boring32::Async::Mutex testMutex;
+			auto testMutex = Boring32::Async::Mutex{};
 			Assert::IsFalse(testMutex.TryUnlock());
 		}
 
 		TEST_METHOD(TestUnlockWhenNotOwnedThrows)
 		{
 			Assert::ExpectException<Boring32::Error::Win32Error>(
-				[]()
-				{
-					Boring32::Async::Mutex testMutex(false, false);
+				[]{
+					auto testMutex = Boring32::Async::Mutex{ false, false };
 					testMutex.Unlock();
 				});
 		}
@@ -126,10 +125,15 @@ namespace Async
 		TEST_METHOD(TestCreateInvalidNamedMutex)
 		{
 			Assert::ExpectException<Boring32::Error::Boring32Error>(
-				[]()
-				{
-					Boring32::Async::Mutex testMutex(false, false, L"");
-				});
+				[]{ auto testMutex = Boring32::Async::Mutex{ false, false, L"" }; });
+		}
+		
+		TEST_METHOD(TestClose)
+		{
+			auto testMutex = Boring32::Async::Mutex{ false, false };
+			Assert::IsNotNull(testMutex.GetHandle());
+			testMutex.Close();
+			Assert::IsNull(testMutex.GetHandle());
 		}
 	};
 }
